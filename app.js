a1bf472ac8ba50f76b03d813b1d3a2da3c050387
kishunsu <gmrmsr_sd@yahoo.co.jp>
2019-06-08 12:45:12 +0900

    app

---
+++ app.js
@@ -0,0 +1,91 @@
+'use strict';
+
+const http = require('http');
+const https = require('https');
+const crypto = require('crypto');
+
+const HOST = 'api.line.me'; 
+const REPLY_PATH = '/v2/bot/message/reply';//リプライ用
+const CH_SECRET = '398049613b63bb85d3372a7880d98a00'; //Channel Secretを指定
+const CH_ACCESS_TOKEN = 'c3gW/2BWdG6GWrQ5z/U4MYErOXdtw/jAVcr4WWAeoNw6P5SvHqQ7fwrIQknrrREBbBwjJbVOcarCfYhTzJ7GRoFvWXJ1Hv201MzIpHAHHY/GszFj9k4iUdFHrSlmBKlvF0gTEOElZGyClqiSBT740AdB04t89/1O/w1cDnyilFU='; //Channel Access Tokenを指定
+const SIGNATURE = crypto.createHmac('sha256', CH_SECRET);
+const PORT = 3000;
+
+/**
+ * httpリクエスト部分
+ */
+const client = (replyToken, SendMessageObject) => {    
+    let postDataStr = JSON.stringify({ replyToken: replyToken, messages: SendMessageObject });
+    let options = {
+        host: HOST,
+        port: 443,
+        path: REPLY_PATH,
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json; charset=UTF-8',
+            'X-Line-Signature': SIGNATURE,
+            'Authorization': `Bearer ${CH_ACCESS_TOKEN}`,
+            'Content-Length': Buffer.byteLength(postDataStr)
+        }
+    };
+
+    return new Promise((resolve, reject) => {
+        let req = https.request(options, (res) => {
+                    let body = '';
+                    res.setEncoding('utf8');
+                    res.on('data', (chunk) => {
+                        body += chunk;
+                    });
+                    res.on('end', () => {
+                        resolve(body);
+                    });
+        });
+
+        req.on('error', (e) => {
+            reject(e);
+        });
+        req.write(postDataStr);
+        req.end();
+    });
+};
+
+http.createServer((req, res) => {    
+    if(req.url !== '/' || req.method !== 'POST'){
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end('こんにちは');
+    }
+
+    let body = '';
+    req.on('data', (chunk) => {
+        body += chunk;
+    });        
+    req.on('end', () => {
+        if(body === ''){
+          console.log('bodyが空です。');
+          return;
+        }
+
+        let WebhookEventObject = JSON.parse(body).events[0];
+        console.log(WebhookEventObject);        
+        //メッセージが送られて来た場合
+        if(WebhookEventObject.type === 'message'){
+            let SendMessageObject;
+            if(WebhookEventObject.message.type === 'text'){
+                SendMessageObject = [{
+                    type: 'text',
+                    text: WebhookEventObject.message.text
+                }];
+            }
+            client(WebhookEventObject.replyToken, SendMessageObject)
+            .then((body)=>{
+                console.log(body);
+            },(e)=>{console.log(e)});
+        }
+
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end('su');
+    });
+
+}).listen(PORT);
+
+console.log(`Server running at ${PORT}`);
